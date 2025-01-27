04e29aca11d630d85ab693a0ba6cf5341f7f94a3
scp38901 <philipp.schwind@oth-regensburg.de>
2025-01-27 18:11:53 +0100

    asl listener for mqtt broker

--- listeners/asl.go
+++ listeners/asl.go
@@ -5,48 +5,88 @@
 package listeners
 
 import (
-	"crypto/tls"
 	"log/slog"
 	"net"
 	"sync"
 	"sync/atomic"
+
+	asl "github.com/Laboratory-for-Safe-and-Secure-Systems/go-asl"
+	asllib "github.com/Laboratory-for-Safe-and-Secure-Systems/go-asl/lib"
 )
 
 const TypeASL = "tcp"
 
 // TCP is a listener for establishing client connections on basic TCP protocol.
-type ASLListener struct { // [MQTT-4.2.0-1]
+type ASLServer struct { // [MQTT-4.2.0-1]
 	sync.RWMutex
-	id      string       // the internal id of the listener
-	address string       // the network address to bind to
-	listen  net.Listener // a net.Listener which will listen for new clients
-	config  Config       // configuration values for the listener
-	log     *slog.Logger // server logger
-	end     uint32       // ensure the close methods are only called once
+	id                string       // the internal id of the listener
+	address           string       // the network address to bind to
+	listen            net.Listener // a net.Listener which will listen for new clients
+	config            Config       // configuration values for the listener
+	log               *slog.Logger // server logger
+	end               uint32       // ensure the close methods are only called once
+	aslEndpointConfig *asl.EndpointConfig
 }
 
 // NewTCP initializes and returns a new TCP listener, listening on an address.
-func NewASLListener(config Config) *ASLListener {
-	return &ASLListener{
-		id:      config.ID,
-		address: config.Address,
-		config:  config,
+func NewASLListener(config Config, aslEndpointConfig asl.EndpointConfig) *ASLServer {
+	aslserver := &ASLServer{
+		id:                config.ID,
+		address:           config.Address,
+		config:            config,
+		aslEndpointConfig: &aslEndpointConfig,
 	}
+
+	return aslserver
 }
-func (l *TCP) Init(log *slog.Logger) error {
+
+func (l *ASLServer) Init(log *slog.Logger) error {
 	l.log = log
 
 	var err error
-	if l.config.TLSConfig != nil {
-		l.listen, err = tls.Listen("tcp", l.address, l.config.TLSConfig)
-	} else {
-		l.listen, err = net.Listen("tcp", l.address)
+
+	endpoint := asl.ASLsetupServerEndpoint(l.aslEndpointConfig)
+
+	addr, err := net.ResolveTCPAddr("tcp", l.address)
+	if err != nil {
+		return err
 	}
+
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	aslListener := asllib.ASLListener{
+		TcpListener: listener,
+		Endpoint:    endpoint,
+	}
+	l.listen = aslListener
 
 	return err
 }
 
-func (l ASLListener) Serve(EstablishFn) {
+// ID returns the id of the listener.
+func (l *ASLServer) ID() string {
+	return l.id
+}
+
+// Address returns the address of the listener.
+func (l *ASLServer) Address() string {
+	if l.listen != nil {
+		return l.listen.Addr().String()
+	}
+	return l.address
+}
+
+// Protocol returns the address of the listener.
+func (l *ASLServer) Protocol() string {
+	return "tls"
+}
+
+// Serve starts waiting for new TCP connections, and calls the establish
+// connection callback for any received.
+func (l ASLServer) Serve(establish EstablishFn) {
 	for {
 		if atomic.LoadUint32(&l.end) == 1 {
 			return
@@ -68,27 +108,15 @@ func (l ASLListener) Serve(EstablishFn) {
 	}
 }
 
-func (l ASLListener) ID() string {
-
-	return l.id
-}
-func (l ASLListener) Address() string {
-	if l.listen != nil {
-		return l.listen.Addr().String()
-	}
-	return l.address
-}
-
-func (l ASLListener) Protocol() string {
-	return "tls"
-}
-func (l ASLListener) Close(CloseFn) {
+// Close closes the listener and any client connections.
+func (l *ASLServer) Close(closeClients CloseFn) {
 	l.Lock()
 	defer l.Unlock()
 
 	if atomic.CompareAndSwapUint32(&l.end, 0, 1) {
 		closeClients(l.id)
 	}
+
 	if l.listen != nil {
 		err := l.listen.Close()
 		if err != nil {
